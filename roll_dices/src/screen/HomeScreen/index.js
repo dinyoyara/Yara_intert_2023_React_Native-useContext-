52d8114e26acb89e905c32540308155ad911917c
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-30 17:23:39 +0300

    Connect Home screen with store, add functionalities.

--- roll_dices/src/screen/HomeScreen/index.js
+++ roll_dices/src/screen/HomeScreen/index.js
@@ -1,51 +1,80 @@
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Button from '../../components/Button';
 import Dice from '../../components/Dice';
+import { rollDices, clearRolls } from '../../store';
 
 import styles from './styles';
 
 const HomeScreen = () => {
-    const [first, setFirst] = useState(6);
-    const [second, setSecond] = useState(6);
-    const [third, setThird] = useState(6);
-    const [fourth, setFourth] = useState(6);
-    const [rolled, setRolled] = useState(false);
+    const dispatch = useDispatch();
+    const rolls = useSelector((state) => {
+        console.log(state.rolls);
+        return state.rolls;
+    });
+    const dices = useSelector((state) => {
+        console.log(state.dices);
+        return state.dices;
+    });
 
-    function getRandomNumber(max) {
-        return Math.floor(1 + Math.random() * max);
-    }
+    const getRandomNumber = (diceSides) => {
+        return Math.floor(1 + Math.random() * diceSides);
+    };
+    const getDicesResultArray = (dicesNumber, diceSides) => {
+        const arr = [];
+        for (let i = 0; i < dicesNumber; i++) {
+            arr.push(getRandomNumber(diceSides));
+        }
+        return arr;
+    };
+    const getBestResult = (rollsInfo) => {
+        let result = 0;
+        rollsInfo.forEach((x) => {
+            result = getArraySum(x) > result ? getArraySum(x) : result;
+        });
+        return result;
+    };
+    const getArraySum = (arr) => {
+        return arr.reduce((x, y) => x + y, 0);
+    };
 
-    const rollDices = () => {
-        setFirst(getRandomNumber(6));
-        setSecond(getRandomNumber(6));
-        setThird(getRandomNumber(6));
-        setFourth(getRandomNumber(6));
-        setRolled(true);
+    const roll = () => {
+        dispatch(rollDices(getDicesResultArray(dices.dicesNumber, dices.diceSides)));
     };
 
     const restartGame = () => {
-        setRolled(false);
-        setFirst(6);
-        setSecond(6);
-        setThird(6);
-        setFourth(6);
+        dispatch(clearRolls());
     };
+
+    const dicesIndexArray = [];
+
+    for (let i = 0; i < dices.dicesNumber; i++) {
+        dicesIndexArray.push(i);
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Dices App</Text>
-            <View style={styles.dices_container}>
-                <Dice number={first} />
-                <Dice number={second} />
-                <Dice number={third} />
-                <Dice number={fourth} />
+            <View>
+                <Text style={styles.title}>Roll Dices App</Text>
+                {rolls.length > 0 ? (
+                    <Text style={styles.title}>
+                        {rolls.length} rolls - best shot result : {getBestResult(rolls)} /
+                        {dices.diceSides * dices.dicesNumber}
+                    </Text>
+                ) : null}
             </View>
-            {rolled ? (
-                <Text style={{ ...styles.title, ...styles.result }}>{first + second + third + fourth} points</Text>
-            ) : null}
-            <View style={styles.buttons_container}>
-                <Button title='Roll' click={rollDices} />
+            <View style={styles.dices_container}>
+                {dicesIndexArray.map((x) => (
+                    <Dice key={x} number={rolls.length > 0 ? rolls[rolls.length - 1][x] : dices.diceSides} />
+                ))}
+            </View>
+            <View>
+                {rolls.length > 0 ? (
+                    <Text style={styles.title}>{getArraySum(rolls[rolls.length - 1])} points</Text>
+                ) : null}
+                <Button title='Roll' click={roll} />
                 <Button title='Restart' click={restartGame} />
             </View>
         </View>
