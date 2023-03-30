ae33db791d4766a616c237f583cb7dd1f4eabb82
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-30 14:28:19 +0300

    Implement Dice component.

---
+++ roll_dices/src/components/Dice/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        width: 80,
+        height: 80,
+        borderRadius: 8,
+        backgroundColor: 'rgba(218,165,32, 1)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    number: {
+        fontSize: 40,
+        fontWeight: 900,
+        color: 'rgba(34,139,34, 1)'
+    }
+});
+
+export default styles;
